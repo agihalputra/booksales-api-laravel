87448f6af26101cb6014f6ab03f7737a3b6c1cb4
Agihal Putra <146146734+agihalputra@users.noreply.github.com>
2025-10-21 17:31:14 +0700

    Tugas 6 Laravel

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
@@ -11,21 +12,30 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| Di sini kamu bisa mendaftarkan route API untuk aplikasi kamu.
+| Semua route ini otomatis menggunakan prefix "/api" dan middleware group "api".
 |
 */
 
+// Route default sanctum
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Auth routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
-Route::apiResource('/books', BookController::class);
+Route::apiResource('/books', BookController::class)->only('index','show');
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::apiResource('/authors', AuthorController::class);
+    Route::apiResource('/genres', GenreController::class);
+
+    Route::middleware(['role:admin'])->group(function () {
+        Route::apiResource('/books', BookController::class)->only('store','update', 'destroy');
+    });
+});
 
 
-Route::apiResource('/genres', GenreController::class);
-
-
-Route::apiResource('/authors', AuthorController::class);
